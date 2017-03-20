aa7ad7326e45c9f80b5b714039118f71b882111e
BadGuy <badguy5525@gmail.com>
2017-03-20 03:54:23 -0300

    Update no script de mostrar informações do sistema.

--- system/system.sh
+++ system/system.sh
@@ -1,6 +1,6 @@
-#!/bin/bash 
+ #!/bin/bash
 
-if [ ! /proc/cpuinfo ]
+if [[ ! /proc/cpuinfo && ! $(which dmidecode) ]]
 then
 echo "Error Getting System Information."
 exit 0
@@ -17,9 +17,17 @@ exit 0
 fi
 
 system=$(cat /etc/issue.net)
+boardmanufacture=$(sudo dmidecode -t baseboard | grep "Manufacturer:" | awk -F ":" '{print $2}')
+productname=$(sudo dmidecode -t baseboard | grep "Product Name:" | awk -F ":" '{print $2}')
 based=$(cat /etc/*release | grep ID_LIKE | awk -F "=" '{print $2}')
 processor=$(cat /proc/cpuinfo | grep "model name" | uniq | awk -F ":" '{print $2}')
 cpus=$(cat /proc/cpuinfo | grep processor | wc -l)
+voltageprocessor=$(sudo dmidecode -t processor | grep Voltage | awk '{print $2" "$3}')
+familyprocessor=$(sudo dmidecode -t processor | grep Family: | awk '{print $2" "$3}')
+processortype=$(sudo dmidecode -t processor | grep Type: | awk '{print $2" "$3}')
+
+echo "Board Manufacturer: $boardmanufacture"
+echo "Product Name: $productname"
 
 if [ "$system" ]
 then
@@ -47,15 +55,30 @@ echo "Clock: $clock MHz"
 else
 echo "Clock: Not Available"
 fi
+echo "Processor Voltage: $voltageprocessor"
+echo "Processor Type: $processortype"
+echo "Processor Family: $familyprocessor" 
 echo "$(ps aux  | awk 'BEGIN { sum = 0 }  { sum += sprintf("%f",$3) }; END { printf "CPU Usage: " "%.2f" "%%", sum}')"
 totalram=$(free | grep Mem | awk '{print $2}')
 usedram=$(free | grep Mem | awk '{print $3}')
 freeram=$(free | grep Mem | awk '{print $4}')
-swapram=$( cat /proc/meminfo | grep SwapTotal | awk '{print $2}')
+swapram=$(cat /proc/meminfo | grep SwapTotal | awk '{print $2}')
+typeram=$(dmidecode -t memory | grep "Type: " | grep -v "Error" | uniq | awk '{print $2}')
+slotsram=$(sudo dmidecode -t memory | grep Size | wc -l)
+
 echo "RAM TOTAL: $(($totalram / 1024))MB USED: $(($usedram / 1024))MB FREE: $(($freeram / 1024))MB SWAP: $(($swapram / 1024))MB "
+echo "Type of RAM: $typeram"
+echo "Number of RAM Slots: $slotsram"
+echo "$(sudo dmidecode -t memory | grep Size -n  | awk '{print $3" "$4" "$5}')" > /tmp/raminfo
+i=0
+while read value
+do
+i=$(($i + 1))
+echo Slot "$i": "$value"
+done < /tmp/raminfo
+
 echo "Uptime: $(uptime)"
 echo "Hostname: $(hostname)"
 echo "IP: $(ip addr | grep inet | grep -v inet6 | grep -v "host lo" | awk '{print $2}' | awk -F "/" '{print $1}')"
 echo "Kernel Version: $(uname -r)"
 echo "Architecture: $(uname -m)"
-exit 1
