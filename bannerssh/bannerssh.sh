70251a2df7033d5e6c747f7890e44b7cd2804b64
BadGuy <badguy5525@gmail.com>
2017-04-06 22:31:08 -0300

    Update bannerssh.sh

--- bannerssh/bannerssh.sh
+++ bannerssh/bannerssh.sh
@@ -2,7 +2,7 @@
 
 
 read -p "Digite sua mensagem para o Banner: " mensagem
-
+echo "$mensagem" > /etc/BadManager/bannerssh/banner.txt
 echo "Banner /etc/BadManager/bannerssh/banner.txt" >> /etc/ssh/sshd_config
 
 service ssh restart 
